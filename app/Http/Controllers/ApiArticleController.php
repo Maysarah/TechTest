cdb9a69caeae58398fb71a2e97768c5c169cfbae
blogTeam22 <abualoon@yahoo.com>
2024-07-30 15:00:26 +0200

    update

--- app/Http/Controllers/ApiArticleController.php
+++ app/Http/Controllers/ApiArticleController.php
@@ -3,17 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Http\Requests\StoreArticleRequestApi;
 
 class ApiArticleController extends Controller
 {
+    /**
+     * Display a listing of the articles.
+     *
+     * @return JsonResponse
+     */
     public function index()
     {
         $articles = Article::all();
         return response()->json($articles);
     }
 
+    /**
+     * Store a newly created article in storage.
+     *
+     * @param StoreArticleRequestApi $request
+     * @return JsonResponse
+     */
     public function store(StoreArticleRequestApi $request)
     {
         // Create the article
@@ -23,17 +35,36 @@ class ApiArticleController extends Controller
         return response()->json($article, 201);
     }
 
+    /**
+     * Display the specified article.
+     *
+     * @param Article $article
+     * @return JsonResponse
+     */
     public function show(Article $article)
     {
         return response()->json($article);
     }
 
+    /**
+     * Update the specified article in storage.
+     *
+     * @param StoreArticleRequestApi $request
+     * @param Article $article
+     * @return JsonResponse
+     */
     public function update(StoreArticleRequestApi $request, Article $article)
     {
         $article->update($request->only(['title', 'content']));
         return response()->json($article);
     }
 
+    /**
+     * Remove the specified article from storage.
+     *
+     * @param Article $article
+     * @return JsonResponse
+     */
     public function destroy(Article $article)
     {
         $article->delete();
--- app/Http/Controllers/WebArticleController.php
+++ app/Http/Controllers/WebArticleController.php
@@ -5,27 +5,50 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreArticleRequestWeb;
 use App\Models\Article;
 use App\Services\ImageService;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 
 class WebArticleController extends Controller
 {
     protected $imageService;
 
+    /**
+     * Create a new controller instance.
+     *
+     * @param  \App\Services\ImageService  $imageService
+     */
     public function __construct(ImageService $imageService)
     {
         $this->imageService = $imageService;
     }
 
+    /**
+     * Display a listing of the articles.
+     *
+     * @return View
+     */
     public function index()
     {
         $articles = Article::with('images')->get(); // Include images when fetching articles
         return view('articles.index', compact('articles'));
     }
 
+    /**
+     * Show the form for creating a new article.
+     *
+     * @return View
+     */
     public function create()
     {
         return view('articles.create');
     }
 
+    /**
+     * Store a newly created article in storage.
+     *
+     * @param  \App\Http\Requests\StoreArticleRequestWeb  $request
+     * @return RedirectResponse
+     */
     public function store(StoreArticleRequestWeb $request)
     {
         // Validate and create the article
@@ -39,18 +62,37 @@ class WebArticleController extends Controller
         return redirect()->route('articles.index')->with('success', 'Article created successfully.');
     }
 
+    /**
+     * Display the specified article.
+     *
+     * @param Article $article
+     * @return View
+     */
     public function show(Article $article)
     {
         $article->load('images'); // Include images when showing an article
         return view('articles.show', compact('article'));
     }
 
+    /**
+     * Show the form for editing the specified article.
+     *
+     * @param Article $article
+     * @return View
+     */
     public function edit(Article $article)
     {
         $article->load('images'); // Include images when editing an article
         return view('articles.edit', compact('article'));
     }
 
+    /**
+     * Update the specified article in storage.
+     *
+     * @param  \App\Http\Requests\StoreArticleRequestWeb  $request
+     * @param Article $article
+     * @return RedirectResponse
+     */
     public function update(StoreArticleRequestWeb $request, Article $article)
     {
         // Update the article
@@ -64,6 +106,12 @@ class WebArticleController extends Controller
         return redirect()->route('articles.index')->with('success', 'Article updated successfully.');
     }
 
+    /**
+     * Remove the specified article from storage.
+     *
+     * @param Article $article
+     * @return RedirectResponse
+     */
     public function destroy(Article $article)
     {
         // Delete associated images from S3
